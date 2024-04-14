de6c9bd99403265e6c6cf3a8d4ac3ac95de09a56
pjw0194 <pjw019472@gmail.com>
2024-04-13 23:04:30 -0500

    map

--- djangoreactapi/frontend/src/MapComponent.js
+++ djangoreactapi/frontend/src/MapComponent.js
@@ -1,14 +1,29 @@
 import React, { useEffect } from "react";
 
-function MapComponent() {
+function MapComponent({ locations }) {
   useEffect(() => {
     // 지도 로딩 시나리오 함수 정의
     if (!window.loadMapScenario) {
       window.loadMapScenario = function () {
         var map = new window.Microsoft.Maps.Map(
           document.getElementById("myMap"),
-          {}
+          {
+            credentials:
+              "ArT-C0vh0XjKS-ED1MLpcfBoxza_5KOS1256vRj6eZd30BYVp9dGbpGgvJYI5nGX",
+            center: new window.Microsoft.Maps.Location(47.6062, -122.3321),
+            zoom: 10,
+          }
         );
+
+        // locations.forEach((location) => {
+        //   var loc = new window.Microsoft.Maps.Location(
+        //     location.latitude,
+        //     location.longitude
+        //   );
+        //   var pin = new window.Microsoft.Maps.Pushpin(loc);
+        //   map.entities.push(pin);
+        // });
+
         // 지도 클릭 이벤트 리스너 추가
         window.Microsoft.Maps.Events.addHandler(map, "click", function (e) {
           var point = new window.Microsoft.Maps.Point(e.getX(), e.getY());
