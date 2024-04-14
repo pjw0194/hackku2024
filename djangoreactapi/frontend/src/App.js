d4a2f885154d2b2a5fffbe6b886080fe7b883f2e
pjw0194 <pjw019472@gmail.com>
2024-04-13 19:51:15 -0500

    update for api

--- djangoreactapi/frontend/src/App.js
+++ djangoreactapi/frontend/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [businesses, setBusinesses] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/api/businesses").then((response) => {
+      console.log(response.json());
+    });
+  }, []);
+};
 
 export default App;
---
+++ djangoreactapi/post/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+
+from .models import Post
+from .serializers import PostSerializer
+from django.http import JsonResponse
+
+from django.conf import settings
+
+import requests, json
+
+@api_view(['GET'])
+def businesses(request):
+    API_KEY = settings.API_KEY
+    url = 'https://api.yelp.com/v3/businesses/search'
+    headers = {
+        'Authorization': f'Bearer {API_KEY}',
+    }
+    params = {
+        'location': 'NYC',
+        'sort_by': 'best_match',
+        'limit': 20
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+
+    return JsonResponse(data)
+
+class ListPost(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class DetailPost(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
